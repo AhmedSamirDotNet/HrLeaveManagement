c065da64b21b739e5afdde277018363822c3993c
AhmedSamirDotNet <ahmedsamirr912@gmil.com>
2025-09-29 12:46:42 +0300

    MainStructre

---
+++ HrLeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hr.LeaveManagement.Domain
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+        public DateTime StarDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string RequestComments { get; set; }
+        public DateTime DateActioned { get; set; }
+        public bool? Approved { get; set; }
+        public bool Cancelled { get; set; }
+
+    }
+}
